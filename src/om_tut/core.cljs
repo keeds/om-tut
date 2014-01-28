cdd5c90233d4e86055bc10189cc74efa7dea8f67
Andrew Keedle <andrew@numptyheads.com>
2014-01-28 22:21:01 +0000

    final tutorial stuff

--- src/om_tut/core.cljs
+++ src/om_tut/core.cljs
@@ -8,17 +8,31 @@
 
 (enable-console-print!)
 
-;; (def app-state (atom {:list ["Lion" "Zebra" "Buffalo" "Antelope"]}))
+;; (def app-state
+;;   (atom
+;;     {:contacts
+;;      [{:first "Ben" :last "Bitdiddle" :email "benb@mit.edu"}
+;;       {:first "Alyssa" :middle-initial "P" :last "Hacker" :email "aphacker@mit.edu"}
+;;       {:first "Eva" :middle "Lu" :last "Ator" :email "eval@mit.edu"}
+;;       {:first "Louis" :last "Reasoner" :email "prolog@mit.edu"}
+;;       {:first "Cy" :middle-initial "D" :last "Effect" :email "bugs@mit.edu"}
+;;       {:first "Lem" :middle-initial "E" :last "Tweakit" :email "morebugs@mit.edu"}]}))
 
 (def app-state
   (atom
-    {:contacts
-     [{:first "Ben" :last "Bitdiddle" :email "benb@mit.edu"}
-      {:first "Alyssa" :middle-initial "P" :last "Hacker" :email "aphacker@mit.edu"}
-      {:first "Eva" :middle "Lu" :last "Ator" :email "eval@mit.edu"}
-      {:first "Louis" :last "Reasoner" :email "prolog@mit.edu"}
-      {:first "Cy" :middle-initial "D" :last "Effect" :email "bugs@mit.edu"}
-      {:first "Lem" :middle-initial "E" :last "Tweakit" :email "morebugs@mit.edu"}]}))
+    {:people
+     [{:type :student :first "Ben" :last "Bitdiddle" :email "benb@mit.edu"}
+      {:type :student :first "Alyssa" :middle-initial "P" :last "Hacker"
+       :email "aphacker@mit.edu"}
+      {:type :professor :first "Gerald" :middle "Jay" :last "Sussman"
+       :email "metacirc@mit.edu" :classes [:6001 :6946]}
+      {:type :student :first "Eva" :middle "Lu" :last "Ator" :email "eval@mit.edu"}
+      {:type :student :first "Louis" :last "Reasoner" :email "prolog@mit.edu"}
+      {:type :professor :first "Hal" :last "Abelson" :email "evalapply@mit.edu"
+       :classes [:6001]}]
+     :classes
+     {:6001 "The Structure and Interpretation of Computer Programs"
+      :6946 "The Structure and Interpretation of Classical Mechanics"}}))
 
 (defn parse-contact [contact-str]
   (let [[first middle last :as parts] (string/split contact-str #"\s+")
@@ -31,11 +45,13 @@
                 (>= c 2) (assoc :middle middle)))))
 
 (defn add-contact [app owner]
-  (let [new-contact (-> (om/get-node owner "new-contact")
+  (let [input (om/get-node owner "new-contact")
+        new-contact (-> input
                         .-value
                         parse-contact)]
     (when new-contact
-        (om/transact! app :contacts conj new-contact))))
+      (om/transact! app :contacts conj new-contact)
+      (set! (.-value input) ""))))
 
 (defn middle-name [{:keys [middle middle-initial]}]
   (cond
@@ -53,28 +69,36 @@
                     (dom/span nil (display-name contact))
                     (dom/button #js {:onClick (fn [e] (put! delete @contact))} "Delete")))))
 
+(defn handle-change [e owner {:keys [text]}]
+  (let [value (.. e -target -value)]
+    (if-not (re-find #"[0-9]" value)
+      (om/set-state! owner :text value
+      (om/set-state! owner :text text)))))
+
 (defn contacts-view [app owner]
   (reify
     om/IInitState
     (init-state [_]
-      {:delete (chan)})
+      {:delete (chan)
+       :text ""})
     om/IWillMount
     (will-mount [_]
       (let [delete (om/get-state owner :delete)]
-                       (go (loop []
-                             (let [contact (<! delete)]
-                               (om/transact! app :contacts
-                                             (fn [xs] (vec (remove #(= contact %) xs))))
-                               (recur))))))
+        (go (loop []
+              (let [contact (<! delete)]
+                (om/transact! app :contacts
+                              (fn [xs] (vec (remove #(= contact %) xs))))
+                (recur))))))
     om/IRenderState
     (render-state [this state]
-            (dom/div nil
+                  (dom/div nil
                      (dom/h1 nil "Contact list")
                      (apply dom/ul nil
                             (om/build-all contact-view (:contacts app)
                                           {:init-state state}))
                      (dom/div nil
-                              (dom/input #js {:type "text" :ref "new-contact"})
+                              (dom/input #js {:type "text" :ref "new-contact" :value (:text state)
+                                              :onChange   #(handle-change % owner state)})
                               (dom/button #js {:onClick #(add-contact app owner)} "Add Contact"))))))
 
 
@@ -82,4 +106,41 @@
     (let [st #js {:backgroundColor bgc}]
       (dom/li #js {:style st} text)))
 
-(om/root app-state contacts-view (. js/document (getElementById "contacts")))
+(defn student-view [student owner]
+  (reify
+    om/IRender
+    (render [_]
+            (dom/li nil (display-name student)))))
+
+(defn professor-view [professor owner]
+  (reify
+    om/IRender
+    (render [_]
+            (dom/li nil
+                    (dom/div nil (display-name professor))
+                    (dom/label nil "Classes")
+                    (apply dom/ul nil
+                           (map #(dom/li nil (om/join professor [:classes %]))
+                                (:classes professor)))))))
+
+(defmulti entry-view (fn [person _] (:type person)))
+
+(defmethod entry-view :student
+  [person owner]
+  (student-view person owner))
+
+(defmethod entry-view :professor
+  [person owner]
+  (professor-view person owner))
+
+(defn registry-view [app owner]
+  (reify
+    om/IRenderState
+    (render-state [this state]
+                  (dom/div nil
+                           (dom/h1 nil "Registry")
+                           (dom/ul nil
+                                   (om/build-all entry-view (:people app)))))))
+
+
+(om/root app-state registry-view (. js/document (getElementById "registry")))
